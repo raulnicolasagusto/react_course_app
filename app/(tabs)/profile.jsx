d70508198f20291ddcc75303fd14a41fbd22f5e0
raulnicolasagusto <raulnicolasagusto@gmail.com>
2024-08-31 20:02:40 -0300

    Primer proyecto React Native

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Profile = ()=>{
+    return(
+
+        <View>
+            <Text>
+                te amo bebote
+            </Text>
+        </View>
+
+    )
+
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
---
+++ app/index.jsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, Text, View, Image } from 'react-native';
+import {Link} from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
+
+
+export default function App() {
+  return (
+    
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{
+          height: '100%'
+        }}>
+          <View className="w-full
+             items-center h-full px-4">
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode='contain'
+            />
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[300px]"
+              resizeMode='contain'
+            />
+           <View className="relative mt-5">
+              <Text className="text-3xl text-white font-bold text-center">
+                  Discover Endless Posibilities with{' '}<Text className="text-secondary-200">Aora!</Text>
+              </Text>
+
+           </View>
+
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+  );
+}
+
+
+
+
